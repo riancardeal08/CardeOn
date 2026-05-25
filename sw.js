a18814d9bf019a58801eb6cf114640bbe7db242a
riancardeal08 <riancardeal08@gmail.com>
2026-05-24 21:32:43 -0300

    Implement service worker for caching strategy

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = "fintrack-v1";
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.open(CACHE).then(async cache => {
+
+      try {
+
+        const fresh = await fetch(event.request);
+
+        cache.put(event.request, fresh.clone());
+
+        return fresh;
+
+      } catch {
+
+        return await cache.match(event.request);
+
+      }
+
+    })
+
+  );
+
+});
